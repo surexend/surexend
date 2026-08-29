1aeaa4720c5c6eb05004def1fc71fde6e958d5da
Archsec-Emman <officialemman876@gmail.com>
2026-08-29 21:52:49 +0100

    fix(backend): export CampaignsService, import CampaignsModule in AdminModule

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { BillsModule } from '../bills/bills.module';
+import { CampaignsModule } from '../campaigns/campaigns.module';
 
 @Module({
-  imports: [NotificationsModule, BillsModule],
+  imports: [NotificationsModule, BillsModule, CampaignsModule],
   providers: [AdminService],
   controllers: [AdminController],
   exports: [AdminService],
